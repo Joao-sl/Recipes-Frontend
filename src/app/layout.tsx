b6800b1e70d802ce24ee418398aa62a8ddbb6382
Joao <joao-victor1982@hotmail.com>
2025-05-22 14:28:36 -0300

    Add Prettier configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import "./globals.css";
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Receitolas",
-  description: "Recipe site",
+  title: 'Recipes',
+  description: 'Recipe site',
 };
 
 export default function RootLayout({
@@ -13,9 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <header>Menu</header>
+        <header className='text-9xl text-amber-300'>Menu</header>
 
         {children}
 
