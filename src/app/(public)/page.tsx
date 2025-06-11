a7fb84abbc8db05db9497929f56234fc58879500
Joao <joaosl.dev@gmail.com>
2025-06-11 17:01:25 -0300

    Update .gitignore, package.json for jwt-decode; modify page title in HomePage component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,7 +2,7 @@ import { Loading } from '@/components/Loading';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: `Home - ${process.env.NEXT_PUBLIC_SITE_NAME}`,
+  title: `Home`,
   description:
     'Descubra novas receitas para todas as ocasiões. Inspire-se com dicas culinárias, compartilhe as suas receitas e dicas com o grupo.',
 };
