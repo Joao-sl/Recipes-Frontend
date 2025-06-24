59b4d062bad67b72ab566bd50d326fc7a09b5106
Joao <joaosl.dev@gmail.com>
2025-06-24 11:29:17 -0300

    Refactor layout and profile page to integrate Container and ProfileForm components

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,14 +1,18 @@
+import { Container } from '@/components/Container';
+import { ProfileMenu } from '@/components/ProfileMenu';
+import clsx from 'clsx';
+
 type RootLayoutProps = {
   children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    // TODO: Container Component
-    <div>
-      <nav>TODO: MENU</nav>
-      {children}
-      <footer>TODO: FOOTER</footer>
-    </div>
+    <Container>
+      <div className={clsx('flex flex-col', 'md:grid md:grid-cols-[auto_1fr] md:gap-6')}>
+        <ProfileMenu />
+        {children}
+      </div>
+    </Container>
   );
 }
--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,3 +1,52 @@
-export default function ProfilePage() {
-  return 'TODO: Profile';
+import clsx from 'clsx';
+import { ProfileForm } from '@/components/ProfileForm';
+import { Metadata } from 'next';
+import { getAccessToken, getRefreshToken } from '@/lib/auth/manage-user-session';
+import { PUBLIC_SITE_URL } from '@/lib/config';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Perfil',
+};
+
+export default async function ProfilePage() {
+  const refresh = await getRefreshToken();
+  const access = await getAccessToken();
+  const url = new URL('/api/me', PUBLIC_SITE_URL);
+  const pageWrapperClasses = clsx(
+    'flex justify-center rounded-md',
+    'sm:py-6 sm:border sm:bg-slate-100 sm:border-slate-200',
+  );
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      cookie: `refreshToken=${refresh}; accessToken=${access}`,
+    },
+    next: {
+      revalidate: 600,
+      tags: ['user-updated'],
+    },
+  });
+
+  if (response.status === 401) {
+    redirect('/login?next=%2Fprofile');
+  }
+
+  if (!response.ok) {
+    console.log(response);
+    return (
+      <div className={pageWrapperClasses}>
+        <ProfileForm />
+      </div>
+    );
+  }
+
+  const profileData = await response.json();
+  return (
+    <div className={pageWrapperClasses}>
+      <ProfileForm initialData={profileData.profile} />
+    </div>
+  );
 }
