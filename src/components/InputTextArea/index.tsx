1866814ed1e5a8c902b319047f1b00203cd127ac
Joao <joao-victor1982@hotmail.com>
2025-06-24 11:24:07 -0300

    Add Container, InputDate, and InputTextArea components for improved form handling

---
+++ src/components/InputTextArea/index.tsx
@@ -0,0 +1,30 @@
+import {
+  getInputBgClass,
+  inputCommonClasses,
+  inputWrapper,
+  labelClasses,
+} from '@/utils/styles/inputStyles';
+import clsx from 'clsx';
+
+type InputTextAreaProps = {
+  id: string;
+  labelText?: React.ReactNode;
+} & React.ComponentProps<'textarea'>;
+
+export function InputTextArea({ id, labelText, ...props }: InputTextAreaProps) {
+  return (
+    <div className={inputWrapper}>
+      {labelText && (
+        <label className={labelClasses} htmlFor={id}>
+          {labelText}
+        </label>
+      )}
+
+      <textarea
+        id={id}
+        {...props}
+        className={clsx(inputCommonClasses, 'min-h-27', getInputBgClass(props.disabled))}
+      ></textarea>
+    </div>
+  );
+}
